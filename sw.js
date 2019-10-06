d0a7dd1c42fa82eafee951d0263d9018cd03d63b
Rob <robleroy@msn.com>
2019-10-06 17:56:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const staticCacheName = "static-cache-v1";
-const dynamicCacheName = "dynamic-cache-v1";
+const staticCacheName = "static-cache-v2";
+const dynamicCacheName = "dynamic-cache-v2";
 const assets = [
 	'/',
 	'/index.html',
