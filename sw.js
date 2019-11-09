d98b98dad4849af1d08f77bacbc6f7fbae8d4ca4
Rob <robleroy@msn.com>
2019-11-09 17:43:36 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,30 @@
 // Radio service worker
 
-const CACHE = "radio-cache-v0.1";
+const Current_Cache = "radio-cache-v0.2";
 const offlinePage = "offline.html";
-const cacheWhitelist = [CACHE];
+const cacheWhitelist = [Current_Cache];
 
-// Install stage: set up and open a new cache
-self.addEventListener("install", function (event) {
-  console.log("Install Event ...");
-
+self.addEventListener('install', (event) => {
+  console.log("Install Event :: ");
   event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      return cache.add(offlinePage);
-      console.log("Installed cache: ", CACHE);
+    caches.open(Current_Cache).then((cache) => {
+      return cache.addAll([
+        cacheWhitelist
+      ]);
+      console.log("Installed cache: ", Current_Cache.toString);
     })
   );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log("activate: ");
-  // Deletes all caches not included in whitelist - 
-  // ie, change in version number etc.
+  console.log("Activate Event :: ");
+
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-            console.log("Deleted cache: ", cacheName);
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            return caches.delete(key);
+            console.log("Deleted cache: ", key);
           }
         })
       );
@@ -35,11 +33,13 @@ self.addEventListener('activate', function(event) {
 });
 
 
+
 // If any fetch fails, it will show the c
 self.addEventListener("fetch", function (event) {
+  console.log("Fetch Event :: ");
 
   if (event.request.destination === "audio") {
-    console.log('audio');
+    console.log('audio intercept...');
     return;
   };  
   
@@ -57,20 +57,21 @@ self.addEventListener("fetch", function (event) {
       
       console.error("Network request Failed. Serving offline page " + error);
       // load offline page
-      return caches.open(CACHE).then(function (cache) {
+      return caches.open(Current_Cache).then(function (cache) {
         return cache.match(offlinePage);
       });
     })
   );
 });
 
-
 // This is an event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener("refreshOffline", function () {
+  console.log("RefreshOffline Event :: ");
+
   const offlinePageRequest = new Request(offlinePage);
 
   return fetch(offlinePage).then(function (response) {
-    return caches.open(CACHE).then(function (cache) {
+    return caches.open(Current_Cache).then(function (cache) {
       console.log("Offline page updated from refreshOffline event: " + response.url);
       return cache.put(offlinePageRequest, response);
     });
